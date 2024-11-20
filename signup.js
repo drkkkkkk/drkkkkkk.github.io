4790b236232e251f059eda322440e0fd78889233
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-11-20 22:23:32 +0000

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,25 +1,20 @@
 import { auth } from './firebase-init.js';
 import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js';
 
-const signupBtn = document.getElementById("signup-btn");
-const signupEmailInput = document.getElementById("signup-email-input");
-const signupPasswordInput = document.getElementById("signup-password-input");
+const signupForm = document.getElementById("signup-form");
 
-signupBtn.addEventListener("click", () => {
-  const email = signupEmailInput.value;
-  const password = signupPasswordInput.value;
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("signup-email-input").value;
+  const password = document.getElementById("signup-password-input").value;
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
-      user.sendEmailVerification()  // Send verification email
-        .then(() => {
-          alert('Please check your email for the verification link.');
-          window.location.href = 'index.html';  // Redirect to login
-        })
-        .catch((error) => {
-          alert(error.message);
-        });
+      user.sendEmailVerification().then(() => {
+        alert("Please check your email for the verification link.");
+        window.location.href = "index.html"; // Redirect to login
+      });
     })
     .catch((error) => {
       alert(error.message);
