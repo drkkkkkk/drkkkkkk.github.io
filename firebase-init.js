8a4bfb77e7cda3fe99a723e2929ab5cb06e2a152
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-11-20 22:07:40 +0000

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,18 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV3IOUukiU3a_7coFFKX7DNHHJ8uMswCo",
+  authDomain: "drks-debadges.firebaseapp.com",
+  projectId: "99406338166",
+  storageBucket: "drks-debadges.appspot.com",
+  messagingSenderId: "99406338166",
+  appId: "1:99406338166:web:9cb98d74ed0c7856e5a757"
+};
+
+// Initialize Firebase
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js';
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };  // Export the auth instance for use in other files
