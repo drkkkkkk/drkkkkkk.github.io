2649bcfb2ac2b3a8752c1265c8a39ef06a8bd4b5
drk playz <93538336+drkkkkkk@users.noreply.github.com>
2024-11-20 22:19:10 +0000

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,27 +1,15 @@
-// Firebase Configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAV3IOUukiU3a_7coFFKX7DNHHJ8uMswCo",
-  authDomain: "drks-debadges.firebaseapp.com",
-  projectId: "99406338166",
-  storageBucket: "drks-debadges.appspot.com",
-  messagingSenderId: "99406338166",
-  appId: "1:99406338166:web:9cb98d74ed0c7856e5a757"
-};
+import { auth } from './firebase-init.js';
+import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js';
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// DOM Elements
 const signupBtn = document.getElementById("signup-btn");
 const signupEmailInput = document.getElementById("signup-email-input");
 const signupPasswordInput = document.getElementById("signup-password-input");
 
-// Sign-Up Functionality
 signupBtn.addEventListener("click", () => {
   const email = signupEmailInput.value;
   const password = signupPasswordInput.value;
 
-  firebase.auth().createUserWithEmailAndPassword(email, password)
+  createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       const user = userCredential.user;
       user.sendEmailVerification()  // Send verification email
